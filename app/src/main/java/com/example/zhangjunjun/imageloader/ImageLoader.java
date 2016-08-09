e6b36de3529fd45cbfa2ec85df78e8b8d0eee246
zjj <google2720@aliyun.com>
2016-08-09 23:36:15 +0800

    1.完成初始版本

--- app/src/main/java/com/example/zhangjunjun/imageloader/ImageLoader.java
+++ app/src/main/java/com/example/zhangjunjun/imageloader/ImageLoader.java
@@ -1,8 +1,12 @@
 package com.example.zhangjunjun.imageloader;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.LruCache;
+import android.widget.ImageView;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,13 +15,11 @@ import java.util.concurrent.Executors;
  */
 public class ImageLoader {
     LruCache<String,Bitmap> mImageCache;
-    ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-
+    ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     public ImageLoader()
     {
         initImageCache();
     }
-
 
     private void initImageCache() {
         //计算可使用的最大内存
@@ -27,8 +29,41 @@ public class ImageLoader {
         mImageCache = new LruCache<String, Bitmap>(cacheSize){
             @Override
             protected int sizeOf(String key, Bitmap value) {
+                 //重写此方法来衡量每张图片的大小
                 return value.getRowBytes()*value.getHeight()/1024;
             }
         };
+    }
+
+    public void displayImage(final String url, final ImageView imageView)
+    {
+        imageView.setTag(url);
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+              Bitmap bitmap = downloadImage(url);
+                if(bitmap==null) {
+                    return;
+                }
+                if(imageView.getTag().equals(url)) {
+                    imageView.setImageBitmap(bitmap);
+                }
+
+            }
+        });
+    }
+
+    public Bitmap downloadImage(String imageUrl)
+    {
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(imageUrl);
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            bitmap = BitmapFactory.decodeStream(conn.getInputStream());
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
--- app/src/main/java/com/example/zhangjunjun/imageloader/MainActivity.java
+++ app/src/main/java/com/example/zhangjunjun/imageloader/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.zhangjunjun.imageloader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private android.widget.ImageView ivtest;
+    private ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.ivtest = (ImageView) findViewById(R.id.iv_test);
+        imageLoader = new ImageLoader();
+
+        imageLoader.displayImage("http://i1.hoopchina.com.cn/blogfile/201608/09/BbsImg147072721085318_480x320.jpg",ivtest);
+
     }
 }
