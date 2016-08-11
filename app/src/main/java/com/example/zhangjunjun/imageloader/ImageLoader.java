dc5cb999126894b1546eed58694a154683dacd8b
zhangjunjun <zhangjunjun@yunovo.cn>
2016-08-11 18:32:37 +0800

    公司测试提交

--- app/src/main/java/com/example/zhangjunjun/imageloader/ImageLoader.java
+++ app/src/main/java/com/example/zhangjunjun/imageloader/ImageLoader.java
@@ -22,6 +22,7 @@ public class ImageLoader {
     }
 
     private void initImageCache() {
+        
         //计算可使用的最大内存
         final int maxMemory = (int) (Runtime.getRuntime().maxMemory()/1024);
         //取四分之一的可用内存作为缓存
